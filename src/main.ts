558210e294ed4555b470d227da728f0b56bc1a25
ROGSTRIX-DE-EDU\balle <ballesterospereze@gmail.com>
2024-12-01 18:32:13 +0100

    ERROR NG0400

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+import { routes } from './app/app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()) // Configuración para usar HttpClient con Fetch
+  ]
+};
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
