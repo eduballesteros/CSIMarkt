74b1e23f6eb97cfa3c2a18d5713b026a7ab0d732
ROGSTRIX-DE-EDU\balle <ballesterospereze@gmail.com>
2024-12-02 14:11:02 +0100

    Funcionalidad Dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderDashboardComponent } from '../header-dashboard/header-dashboard.component'
-import { FooterComponent } from '../footer/footer.component'
+import { CommonModule } from '@angular/common';
+import { HeaderDashboardComponent } from '../header-dashboard/header-dashboard.component';
+import { ProductService , Product} from '../../services/product.service';
+import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet,HeaderDashboardComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderDashboardComponent, ProductComponent, CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent {
-  title = 'CSIMarkt -Dashboard';
+export class DashboardComponent implements OnInit {
+  title="CSIMarkt -Dashboard"
 
+  products: Product[] = [];  // Especificamos el tipo de datos como Product[]
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    // Llamamos al servicio para obtener los productos al cargar el componente
+    this.productService.getProducts().subscribe(
+      (data) => {
+        this.products = data;  // Asignamos los productos obtenidos al array
+      },
+      (error) => {
+        console.error('Error al cargar los productos', error);
+      }
+    );
+  }
 }
