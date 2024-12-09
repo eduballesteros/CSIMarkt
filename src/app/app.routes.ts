e824607b9b5f58b0c3d8886063c97f6438abd050
ROGSTRIX-DE-EDU\balle <ballesterospereze@gmail.com>
2024-12-09 19:19:30 +0100

    Add show more

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },  // Página de inicio
   { path: 'login', component: LoginComponent },   // Página de login
   { path: 'register', component: RegisterComponent }, // Página de registro
   { path: 'dashboard', component: DashboardComponent }, // Ruta para el dashboard
-
+  { path: 'product/:id', component: ProductItemComponent }  // Ruta para mostrar el producto con el id
 ];
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
   templateUrl: './product-item.component.html',
-  styleUrl: './product-item.component.css'
+  styleUrls: ['./product-item.component.css'],
+  imports: [ CommonModule]
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit {
+  product: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');  // Obtén el ID del producto desde la URL
+
+    if (productId) {
+      // Llama al servicio para obtener el producto con ese ID
+      this.productService.getProductById(productId).subscribe((data) => {
+        this.product = data;  // Asigna el producto recibido a la propiedad
+      });
+    }
+  }
 }
